e3d9ba6147c74c25b49b933bb68c06005303220c
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2023-02-14 22:18:06 +0200

    fix: floating point division

--- VectorManipulation.cpp
+++ VectorManipulation.cpp
@@ -9,7 +9,7 @@ long long getSum(const std::vector<long long>& numbers) {
     return sum;
 }
 
-long long getAvg(const std::vector<long long>& numbers) {
+double getAvg(const std::vector<long long>& numbers) {
     long long sum = getSum(numbers);
     return 1.0 * sum / numbers.size();
 }
