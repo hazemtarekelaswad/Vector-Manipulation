608d165ed8de2cd43a4ead46617b2f79fc19917a
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2023-02-14 22:08:58 +0200

    implement demo in the main function

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+#include "VectorManipulation.h"
+
+int main() {
+    std::vector<long long> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::cout << "Sum: " << getSum(numbers) << std::endl;
+
+    return 0;
+}
